314fbfd1678c54323f7672c0728c29762af182e6
Tedd Arcuri <tedd.arcuri@www.colorado.gov>
2015-12-28 13:58:37 -0700

    Bourbon and gulp sass

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,8 +1,16 @@
 var gulp = require('gulp');
-var react = require('gulp-react');
+var babel = require('gulp-babel');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+  gulp.src('./css/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
  
 gulp.task('default', function () {
-	return gulp.src('app.jsx')
-		.pipe(react())
-		.pipe(gulp.dest('dist'));
+	gulp.watch('./css/sass/**/*.scss', ['sass']);
+	return gulp.src('js/app.js')
+		.pipe(babel())
+		.pipe(gulp.dest('dist'))
 });
