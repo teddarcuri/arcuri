e6f7ed3e4572b1e61d1c1f71e66337a5c547eafc
Tedd Arcuri <tedd.arcuri@www.colorado.gov>
2016-03-07 11:59:47 -0700

    add production express server to server.js

--- server.js
+++ server.js
@@ -1,15 +1,38 @@
+var express = require('express');
+var app = express();
+
 var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config');
 
-new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  hot: true,
-  historyApiFallback: true
-}).listen(3000, '0.0.0.0', function (err, result) {
-  if (err) {
-    console.log(err);
-  }
+var isDevelopment = (process.env.NODE_ENV !== 'production');
+var static_path = path.join(__dirname, 'build');
 
-  console.log('Listening at 0.0.0.0:3000');
-});
+app.use(express.static(static_path))
+	.get('/', function(req, res) {
+		res.sendFile('index.html', {
+			root: static_path
+		});
+	}).listen(process.env.PORT || 8080, function(err) {
+		if (err) { console.log(err) };
+		console.log('Listening at localhost:8080');
+	});
+
+
+if (isDevelopment) {
+
+	var WebpackDevServer = require('webpack-dev-server');
+	var config = require('./webpack.config');
+
+	new WebpackDevServer(webpack(config), {
+	  publicPath: config.output.publicPath,
+	  hot: true,
+	  historyApiFallback: true
+	}).listen(3000, '0.0.0.0', function (err, result) {
+	  if (err) {
+	    console.log(err);
+	  }
+
+	  console.log('Listening at 0.0.0.0:3000');
+	});
+
+}
+
